d00fb1541af75249e5fbabfe49d2a74454c56418
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-14 23:58:32 +0700

    feat(store): add todo from input

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import {createListItem} from "./dom";
 import {todosStore} from "./store";
+import {nanoid} from "nanoid";
 
 const todosEl = document.getElementById('todos')!
+const addTodoBtn = document.getElementById('add-activity-btn')!
+const inputTodoEl = document.getElementById('input-activity')! as HTMLInputElement
 
 todosStore.subscribe(() => {
   const todos = todosStore.getState()
@@ -11,11 +14,15 @@ todosStore.subscribe(() => {
   })
 })
 
-todosStore.dispatch({
-  type: 'ADD',
-  payload: {
-    id: '8989',
-    value: 'Buy milk',
-    isComplete: false
-  }
+addTodoBtn.addEventListener('click', () => {
+  todosStore.dispatch({
+    type: 'ADD',
+    payload: {
+      id: nanoid(),
+      value: inputTodoEl.value,
+      isComplete: false
+    }
+  })
+
+  inputTodoEl.value = ''
 })
